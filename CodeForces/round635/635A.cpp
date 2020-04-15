df64303adbf89b9dd118415689562e4a63dbb777
Sahil <timothygreen573@gmail.com>
2020-04-15 22:59:55 +0530

    CodeForces Round #635 (Div. 2)

---
+++ CodeForces/round635/635A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double db;
+
+const ll mod  = 1e9+7;
+const db eps  = 1e-9 ;
+const ll maxn = 1e5+1;
+const ll inf = LLONG_MAX;
+
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+#define deb(x) cout << #x << " " << x << endl
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll a,b,c,d;
+        cin >> a >> b >> c >> d;
+        cout << b << " "<< c << " " << c << endl;
+    }
+
+    return 0;
+} 
