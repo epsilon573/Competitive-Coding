307afd8417bb958872b9367c61ff3e49ade1855a
Sahil Sharma <timothygreen573@gmail.com>
2022-03-30 01:46:24 +0530

    improved trie

--- Data Structures/trie.cpp
+++ Data Structures/trie.cpp
@@ -1,60 +1,124 @@
+// Author : Epsilon573
+// If it works, don't touch it.
+
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef unsigned long long ull;
 typedef long long ll;
-typedef double db;
+typedef long double ld;
 
-const ll mod = 1e9+7;
-const db eps = 1e-9;
-const ll alpha_size = 26;
+const ll mod  = 1e9+7;
+const ld eps  = 1e-9 ;
+const ll maxn = 1e5+1;
+const ll inf  = 5e18 ;
+const ll minf = -inf ;
 
 #define mp make_pair
 #define pb push_back
+#define endl "\n"
 
-#define deb(x) cout << #x << " " << x << endl
+// Base Character and Alphabet Size
+char base = 'a';
+ll alpha = 26;
 
-struct trie
-{
-	struct trie* node[alpha_size];
-	bool endofstring;
-};
+// Information about Node
+struct Node{
+    ll ending_here;
+    ll passing_here;    
+    vector<ll> idx;
 
-struct trie* createNode()
-{
-    struct trie* newTrie = new trie;
-    for(ll i=0 ; i<alpha_size ; ++i)
-    {
-        newTrie->node[i] = NULL;
+    Node(){
+        idx.assign(alpha,-1);
+        ending_here = 0;
+        passing_here = 0;
     }
-    newTrie->endofstring = false;
-    return newTrie;
+
+    ll& operator[](ll x){
+        return idx[x];
+    }
 };
 
-void insertTrie(struct trie* root , string s)
-{
-    struct trie* curr = root;
-    for(ll i=0 ; i<s.size() ; ++i)
-	{
-		ll idx = s[i]-'a';
-        if(!curr->node[idx])
-          curr->node[idx] = createNode();
-        curr = curr->node[idx];
-	}	
-	curr->endofstring = true;
-}
+struct Trie{
+    vector<Node> v;
 
-bool searchTrie(struct trie* root, string s)
-{
-	struct trie* curr = root;
-	for(ll i=0 ; i<s.size() ; ++i)
-	{
-		ll idx = s[i]-'a';
-        if(!curr->node[idx]) return false;
-        curr = curr->node[idx];
-	}	
-	return curr->endofstring;
+    Trie(){
+        v.emplace_back();
+    }
 
+    void insertString(string s){
+        ll curr_idx = 0;
+        v[curr_idx].passing_here++;
+
+        for(auto c : s)
+        {
+            if(v[curr_idx][c-base]==-1)
+            {
+                v.emplace_back();
+                v[curr_idx][c-base] = (ll)v.size() - 1;
+            }
+
+            curr_idx = v[curr_idx][c-base];
+            v[curr_idx].passing_here++;
+        }
+
+        v[curr_idx].ending_here++;        
+    }
+
+    // Query Trie
+    // Example - Count strings with given prefix
+    ll countStringsWithPrefix(string s){
+        ll curr_idx = 0;
+        for(auto c : s)
+        {
+            if(v[curr_idx][c-base]==-1)
+            {
+                return 0;
+            }
+            else
+                curr_idx = v[curr_idx][c-base];
+        }
+        return v[curr_idx].passing_here;
+    }
+
+    // Example - Count Longest Common Prefix
+    string longestCommonPrefix(ll n){
+        string res = "";
+        ll curr_idx = 0;
+
+        while(1){
+            ll found = false;
+            for(ll i=0 ; i<alpha ; ++i)
+                if(v[v[curr_idx][i]].passing_here==n)
+                {
+                    res.pb(base+i);
+                    curr_idx = v[curr_idx][i];
+                    found = true;
+                    break;
+                }
+
+            if(!found)
+                break;
+        }
+        return res;
+    }
+};
+
+bool solve()
+{
+    ll n;
+    cin >> n;
+    vector<string> v(n);
+    Trie t;
+
+    for(ll i=0 ; i<n ; ++i){
+        cin >> v[i];
+        t.insertString(v[i]);
+    }
+
+    cout << t.longestCommonPrefix(n);
+    
+    return true;    
 }
 
 int main()
@@ -63,27 +127,25 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    #ifndef ONLINE_JUDGE
+    #ifdef EPSILON
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
     #endif
 
+    ll t=1;
 
-    struct trie* rootTrie = createNode(); 
-
-    ll n; string s;
-    cin >> n;
-    
-    while(n--)
+    while(t--)
     {
-    	cin >> s;
-    	insertTrie(rootTrie , s);
+        if(solve())
+        {
+            // cout << "YES" << endl;
+        }
+        else
+        {
+            // cout << "NO" << endl;
+        }
     }
-
-    cin >> s;
-    if(searchTrie(rootTrie, s)) cout << s << " exists" ;
-    else cout << s << " not found";
 
     return 0;
 } 
